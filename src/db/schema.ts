f655d46887490c9b330c1f93c7f1d3244af32716
Tom <tomlaplace0902@gmail.com>
2024-11-27 17:12:35 +0100

    db : add created at on game

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,4 +1,4 @@
-import { boolean, integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { boolean, integer, pgTable, timestamp, varchar } from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users", {
     id: integer().primaryKey().generatedAlwaysAsIdentity(),
@@ -13,4 +13,5 @@ export const gamesTable = pgTable("games", {
     deaths: integer().notNull(),
     assists: integer().notNull(),
     kills: integer().notNull(),
+    createdAt: timestamp().defaultNow().notNull(),
 })
