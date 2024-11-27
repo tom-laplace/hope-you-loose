ba8a721ebda22cb39d12bdaf65a3535e11604f12
Tom <tomlaplace0902@gmail.com>
2024-11-27 17:13:33 +0100

    db: update result name for comprehensiveness

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -9,7 +9,7 @@ export const usersTable = pgTable("users", {
 export const gamesTable = pgTable("games", {
     game_id: integer().notNull(),
     player_id: varchar().references(() => usersTable.riot_puiid).notNull(),
-    result: boolean().notNull(),
+    win: boolean().notNull(),
     deaths: integer().notNull(),
     assists: integer().notNull(),
     kills: integer().notNull(),
