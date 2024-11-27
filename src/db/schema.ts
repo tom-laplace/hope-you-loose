168cae7189421253b0ea998a95bc62d1621288eb
Tom <tomlaplace0902@gmail.com>
2024-11-27 16:56:46 +0100

    add user info from riot api

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -2,5 +2,6 @@ import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users", {
     id: integer().primaryKey().generatedAlwaysAsIdentity(),
-    riot_username: varchar().unique().notNull()
+    riot_username: varchar().unique().notNull(),
+    riot_puiid: varchar().unique().notNull(),
 })
