f6eaca1e386728d55c37db8e8f09e623b6063eb8
Tom <tomlaplace0902@gmail.com>
2024-11-27 17:07:40 +0100

    db: create game table

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,7 +1,16 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { boolean, integer, pgTable, varchar } from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users", {
     id: integer().primaryKey().generatedAlwaysAsIdentity(),
     riot_username: varchar().unique().notNull(),
     riot_puiid: varchar().unique().notNull(),
+})
+
+export const gamesTable = pgTable("games", {
+    game_id: integer().notNull(),
+    player_id: integer().references(() => usersTable.riot_puiid).notNull(),
+    result: boolean().notNull(),
+    deaths: integer().notNull(),
+    assists: integer().notNull(),
+    kills: integer().notNull(),
 })
